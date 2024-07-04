293545f77818d51170ef86bf3942d9df101126d1
Taizya <taizyampalo3@gmail.com>
2024-07-04 15:56:10 +0200

    added validatin for email field

--- modules/forms/formValidations.js
+++ modules/forms/formValidations.js
@@ -1,17 +1,14 @@
+
 function setStyles(styleContainer, isValid) {
   if (isValid == true) {
     const checkElement = styleContainer.querySelector(".fa-circle-check");
-    const exclamationElement = styleContainer.querySelector(
-      ".fa-circle-exclamation",
-    );
+    const exclamationElement = styleContainer.querySelector(".fa-circle-exclamation");
 
     exclamationElement.style.visibility = "hidden";
     checkElement.style.color = "#2dd243";
     checkElement.style.visibility = "visible";
   } else {
-    const exclamationElement = styleContainer.querySelector(
-      ".fa-circle-exclamation",
-    );
+    const exclamationElement = styleContainer.querySelector(".fa-circle-exclamation");
     const checkElement = styleContainer.querySelector(".fa-circle-check");
 
     checkElement.style.visibility = "hidden";
@@ -24,14 +21,47 @@ export const validateUsername = (element, container) => {
   const username = element.value;
   if (username.length >= 3) {
     element.style.border = "2px solid #2dd243";
+
+    const errElement = container.querySelector(".name-err-msg")
+    errElement.style.visibility = "hidden"
+
     setStyles(container, true);
   } else {
     element.style.border = "2px solid #e11";
+
+    const errElement = container.querySelector(".name-err-msg")
+    errElement.innerText = "Enter Atleast three characters"
+    errElement.style.visibility = "visible"
+
     setStyles(container, false);
   }
 };
 
-export const validateEmail = (element, container) => {};
+export const validateEmail = (element, container) => {
+
+    const email = element.value
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    const result = emailPattern.test(email)
+    
+    if (result == true) {
+        
+        const errElement = container.querySelector(".email-err-msg")
+
+        errElement.style.visibility = "hidden"
+        element.style.border = "2px solid #2dd243";
+        
+        setStyles(container, true)
+    } else {
+
+        element.style.border = "2px solid #e11";
+        const errElement = container.querySelector(".email-err-msg")
+
+        errElement.innerText= "Invalid Email adress"
+        errElement.style.visibility = "visible"
+
+        setStyles(container, false)
+    }
+};
 
 export const validatePassword = (element, container) => {};
 
--- modules/forms/forms.js
+++ modules/forms/forms.js
@@ -12,31 +12,35 @@ export function setupForms(element) {
         <div id="form-container">
             <form>
                 <div class="form-group group1" id="username-container">
-                <label>Username</label><br>
-                <input type="text" id="username" placeholder="Enter your name"><br>
-                <i class="fa-solid fa-circle-check"></i>
-                <i class="fa-solid fa-circle-exclamation"></i>
+                    <label>Username</label><br>
+                    <input type="text" id="username" placeholder="Enter your name"><br>
+                    <i class="fa-solid fa-circle-check"></i>
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <small class="name-err-msg">Error Msg</small>
                 </div>
                 
                 <div class="form-group group2" id="email-container">
-                <label>Email</label><br>
-                <input type="emai" id="email-address" placeholder="Enter your email"><br>
-                <i class="fa-solid fa-circle-check"></i>
-                <i class="fa-solid fa-circle-exclamation"></i>
+                    <label>Email</label><br>
+                    <input type="emai" id="email-address" placeholder="Enter your email"><br>
+                    <i class="fa-solid fa-circle-check"></i>
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <small class="email-err-msg">Error Msg</small>
                 </div>
 
                 <div class="form-group group3" id="password-container">
-                <label>Password</label><br>
-                <input type="password" id="password" placeholder="Enter your password"><br>
-                <i class="fa-solid fa-circle-check"></i>
-                <i class="fa-solid fa-circle-exclamation"></i>
+                    <label>Password</label><br>
+                    <input type="password" id="password" placeholder="Enter your password"><br>
+                    <i class="fa-solid fa-circle-check"></i>
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <small class="pswd-err-msg">Error Msg</small>
                 </div>
 
                 <div class="form-group group4" id="password2-container">
-                <label>Retype Password</label><br>
-                <input type="password" id="password-2" placeholder="Enter your password again"><br>
-                <i class="fa-solid fa-circle-check"></i>
-                <i class="fa-solid fa-circle-exclamation"></i>
+                    <label>Retype Password</label><br>
+                    <input type="password" id="password-2" placeholder="Enter your password again"><br>
+                    <i class="fa-solid fa-circle-check"></i>
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <small class="pswd2-err-msg">Error Msg</small>
                 </div>
 
                 <button id="submit-button">Submit</button><br>
@@ -52,14 +56,15 @@ export function setupForms(element) {
   });
 
   const emailElement = document.querySelector("#email-address");
-  emailElement.addEventListener("Input changed:", validateEmail(emailElement));
+  const emailContainer = document.querySelector("#email-container")
+  emailElement.addEventListener("change", () => {
+    validateEmail(emailElement, emailContainer)
+  }) 
 
   const passwordElement = document.querySelector("#password");
   passwordElement.addEventListener("change", validatePassword(passwordElement));
 
   const password2Element = document.querySelector("#password-2");
-  password2Element.addEventListener(
-    "change",
-    validatePassword2(password2Element),
-  );
+  password2Element.addEventListener("change",validatePassword2(password2Element));
+
 }
